7e51ac5f9e8cf462d7a9fb68b0c60a87bb548025
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-25 00:31:50 -0700

    add window load listener and next page button

--- recipes/recipes.js
+++ recipes/recipes.js
@@ -1,17 +1,52 @@
 // IMPORTS
-import { getRecipes } from "../fetch-utils.js";
+import {
+    getMoreRecipes,
+    getRecipes,
+    getRecipesOnPageLoad,
+} from "../fetch-utils.js";
 import { renderRecipes } from "../render-utils.js";
 
 // GET DOM ELEMENTS
 const recipeSearchForm = document.getElementById("recipe-search-form");
 const recipesList = document.getElementById("recipes-list");
 const errorDisplay = document.getElementById("error-display");
+const queryTotalCount = document.getElementById("query-total-count");
+const nextPage = document.getElementById("next-page");
 
 // STATE
 let error = null;
 let recipes = [];
+let paging = {
+    page: "1",
+    nextPage_url: null,
+};
 
 // EVENTS
+window.addEventListener("load", async () => {
+    const recipe = {
+        dish_name: "pizza",
+    };
+
+    let response = await getRecipesOnPageLoad(recipe.dish_name);
+
+    recipes = response;
+
+    displayRecipes();
+});
+
+nextPage.addEventListener("click", async () => {
+    const response = await getMoreRecipes(recipes._links.next.href);
+
+    console.log(recipes);
+
+    (paging.nextPage_url = response._links.next.href), paging.page++;
+
+    recipes = response;
+
+    console.log(paging.page);
+    displayMoreRecipes();
+});
+
 recipeSearchForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(recipeSearchForm);
@@ -21,15 +56,12 @@ recipeSearchForm.addEventListener("submit", async (e) => {
         health_restrictions: formData.get("health-restrictions"),
     };
 
-    const response = await getRecipes(
+    let response = await getRecipes(
         recipe.dish_name,
         recipe.health_restrictions
     );
 
     recipes = response;
-
-    console.log("search form console log", recipes);
-    console.log("search form console log2", recipe.health_restrictions);
 
     displayError();
     displayRecipes();
@@ -38,6 +70,15 @@ recipeSearchForm.addEventListener("submit", async (e) => {
 // DISPLAY
 function displayRecipes() {
     recipesList.innerHTML = "";
+    queryTotalCount.textContent = `Randomly displaying ${recipes.hits.length} of ${recipes.count} results`;
+    for (const recipe of recipes.hits) {
+        const recipeEl = renderRecipes(recipe);
+        recipesList.append(recipeEl);
+    }
+}
+
+function displayMoreRecipes() {
+    queryTotalCount.textContent = `Randomly displaying ${recipes.hits.length} of ${recipes.count} results`;
     for (const recipe of recipes.hits) {
         const recipeEl = renderRecipes(recipe);
         recipesList.append(recipeEl);
