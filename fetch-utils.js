7a196f92bcb3f57e27460b6e5673ef7ff9288923
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-29 14:09:11 -0700

    random updates

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,7 @@
+//
+//
+//
+//
 // EDAMAM API CALLS
 
 const APP_ID = "e850f419";
@@ -51,6 +55,10 @@ export async function getMoreRecipes(nextURL) {
     return data;
 }
 
+//
+//
+//
+//
 // SUPABASE CALLS
 
 const SUPABASE_URL = "https://csmavwimvjsxbxwvwfxk.supabase.co";
@@ -58,6 +66,7 @@ const SUPABASE_KEY =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzbWF2d2ltdmpzeGJ4d3Z3ZnhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTY5OTYsImV4cCI6MTk4MjE3Mjk5Nn0.G079_-YncXBbnPxw7YR8mtpL5rTWtUFmWrS8uGwtGo8";
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+//
 /* Auth related functions */
 
 export function getUser() {
@@ -81,3 +90,6 @@ export async function signInUser(email, password) {
 export async function signOutUser() {
     return await client.auth.signOut();
 }
+
+//
+/* DATA FUNCTIONS */
